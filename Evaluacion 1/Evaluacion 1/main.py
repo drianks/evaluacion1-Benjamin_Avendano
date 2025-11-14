aa225048fa54503b0f4ea242ad978e041bca355e
drianks <benjamin.avendano04@inacapmail.cl>
2025-11-14 18:45:17 -0300

    cambios finales

--- Evaluacion 1/Evaluacion 1/main.py
+++ Evaluacion 1/Evaluacion 1/main.py
@@ -30,14 +30,23 @@ biblioteca.mostrar_catalogo()
 #------------------------------------------------------------------------------#
 from Parte2 import Curso
 curso = Curso("Programación Orientada a Objetos")
-curso.inscribir_alumno("Benjamín Abendaño")
-curso.inscribir_alumno("Pedrito Pascal")
-curso.inscribir_alumno("Edgard Parra")
+curso.inscribir_alumno("Pedrito pascal")
+curso.inscribir_alumno("Benjamin Avendaño")
+curso.inscribir_alumno("Francisca torre")
 curso.inscribir_alumno("Lilian Labbé")
 
 print("Estado inicial del curso:")
 print(curso.estado_curso())
 
+curso.remover_alumno("Lilian Labbé")
+
+print("Estado después de remover un alumno:")
+print(curso.estado_curso())
+
+# Intento de remover alumno no inscrito
+curso.remover_alumno("Camila Soto")
+print("Estado final del curso:")
+print(curso.estado_curso())
 curso.remover_alumno("Pedrito Pascal")
 
 print("Estado después de quitar al alumno:")
@@ -49,18 +58,107 @@ print(curso.estado_curso())
 #Fin del ejercicio 2 suuuu
 #------------------------------------------------------------------------------#
 from Parte3 import Pedido
+
 pedido = Pedido()
-pedido.agregar_item("camiseta", 29990,2)
-pedido.agregar_item("Pantalones de vestir",59990, 1)
-pedido.agregar_item("Zapatos", 89990, 1)
+pedido.agregar_item("Camisa", 15990, 2)
+pedido.agregar_item("Pantalones", 39990, 1)
+pedido.agregar_item("Zapatos", 79990, 1)
 
 print("Detalle del pedido:")
 pedido.listar_items()
 
 total = pedido.total_pedido()
 print(f"Total a pagar: {total}")
-#Fin del ejercicio 3 \uwu/
+# Fin del tercer ejercicio \uwu/
 #------------------------------------------------------------------------------#
+from Parte4 import Sensor
+# Probar el sistema de sensor de mediciones ambientales
+nombre_sensor = input("Ingrese el nombre del sensor: ")
+sensor = Sensor(nombre_sensor)
+
+while True:
+    valor = input("Ingrese una medición (o 'fin' para terminar): ")
+    if valor.lower() == "fin":
+        break
+    else:
+        sensor.agregar_medicion(float(valor))
+sensor.mostrar_resumen()
+# Fin del ejercicio 4 _-(-.-)_--
+#------------------------------------------------------------------------------#
+from Parte5 import Catalogo
+
+# Ejemplo de uso
+
+catalogo = Catalogo()
+catalogo.registrar_pelicula("Tron", "Ciencia Ficción", 2010)
+catalogo.registrar_pelicula("El niño con el pijama de rayas", "Drama", 2008)
+catalogo.registrar_pelicula("V de venganza", "Acción / Drama", 2006)
+catalogo.registrar_pelicula("kung fu panda", "Ciencia Ficción ", 2008)
+catalogo.registrar_pelicula("Demon slayer", "Animacion", 2025)
+catalogo.registrar_pelicula("jujutsu kaisen pelicula", "Ciencia Ficción / animacion", 2021)
+
+print("Catálogo completo:")
+catalogo.mostrar_catalogo()
+
+titulo_buscar = "Inception"
+pelicula_encontrada = catalogo.buscar_pelicula(titulo_buscar)
+if pelicula_encontrada:
+    print(f"\nPelícula encontrada: {pelicula_encontrada.informacion()}")
+else:
+    print(f"\nLa película '{titulo_buscar}' no se encuentra en el catálogo.")
+
+genero_filtrar = "Ciencia Ficción"
+peliculas_filtradas = catalogo.filtrar_por_genero(genero_filtrar)
+if peliculas_filtradas:
+    print(f"\nPelículas del género '{genero_filtrar}':")
+    for pelicula in peliculas_filtradas:
+        print(pelicula.informacion())
+else:
+    print(f"\nNo se encontraron películas del género '{genero_filtrar}'.")
+#Fin del código
+#Fin de la parte cinco (*-*)
+#------------------------------------------------------------------------------#
+from parte6 import SistemaUsuarios
+
+# Modo de uso del sistema de usuarios y autenticación simple
+sistema = SistemaUsuarios()
+
+# Registrar usuarios
+sistema.registrar_usuario("Lolito", "1234")
+sistema.registrar_usuario("Peditro", "abcd")
+sistema.registrar_usuario("Lolito", "9999")  # Intento duplicado
+
+# Iniciar sesión
+sistema.iniciar_sesion("Lolito", "1234")     # Correcto
+sistema.iniciar_sesion("Juanito", "1234")     # Contraseña incorrecta
+sistema.iniciar_sesion("Pedrito", "qwerty")  # Usuario no existe
+
+# Consultar si un usuario existe
+sistema.usuario_registrado("Pedrito")
+sistema.usuario_registrado("Juanito")
+
+#Fin del ejercicio 6 (^_^)
+#------------------------------------------------------------------------------#
+from Parte7 import Agenda
+agenda = Agenda()
+print(agenda.agregar_contacto("Juanito", "1234567890", "juanito@mail.cl"))
+print(agenda.agregar_contacto("Benjamin Avendaño", "9876543210", "benjamin@mail.cl"))
+print(agenda.agregar_contacto("Pedrito", "1112223333", "Pedrito@mail.cl"))
+print("\nListado de contactos:")
+agenda.mostrar_contactos()
+print("\nBúsqueda de contacto 'Juanito':")
+print(agenda.buscar_contacto("Juanito"))
+print("\nBúsqueda de contacto 'Franciasca torre':")
+print(agenda.buscar_contacto("Franciasca torre"))
+print("\nEliminación de contacto 'Benjamin Avendaño':")
+print(agenda.eliminar_contacto("Benjamin Avendaño"))
+print("\nEliminación de contacto 'Francisca torre':")
+print(agenda.eliminar_contacto("Francisca torre"))
+print("\nListado de contactos actualizado:")
+agenda.mostrar_contactos()
+#Fin del código y todos los ejercicios
+
+
 
 
 
